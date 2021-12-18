8d339d68599c47bfda4feda1b32d7497e48025e0
Dana <sundetbayeva.dana@yandex.ru>
2021-12-18 15:57:04 +0500

    try to do cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -10,15 +10,15 @@ module.exports = (req, res, next) => {
   console.log(origin);
   const { method } = req;
   console.log(method);
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
+  // const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  // const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    if (method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-      return res.end();
-    }
+    // if (method === 'OPTIONS') {
+    //   res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    //   res.header('Access-Control-Allow-Headers', requestHeaders);
+    //   return res.end();
+    // }
   }
   next();
 };

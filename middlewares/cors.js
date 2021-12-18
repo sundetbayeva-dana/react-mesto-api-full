be7cc9ab9ae4c8137d25a140e20d503919660acf
Dana <sundetbayeva.dana@yandex.ru>
2021-12-18 19:43:45 +0500

    do cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -6,18 +6,25 @@ const allowedCors = [
   'localhost:3000',
 ];
 module.exports = (req, res, next) => {
+  console.log('заново');
+  console.log('AAA');
   const { origin } = req.headers;
   const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  console.log(origin);
+  const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
+    console.log('not opt');
+    console.log(origin);
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', 'true');
+    if (method === 'OPTIONS') {
+      console.log('options');
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+      res.header('Access-Control-Allow-Headers', requestHeaders);
+      return res.end();
+    }
   }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
+
   return next();
 };

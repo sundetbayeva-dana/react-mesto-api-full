47955fd269729d40ff1f8cbac0b8c513d6621192
Dana <sundetbayeva.dana@yandex.ru>
2021-12-18 18:43:23 +0500

    try to do cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -6,21 +6,17 @@ const allowedCors = [
   'localhost:3000',
 ];
 module.exports = (req, res, next) => {
-  console.log(req.headers);
   const { origin } = req.headers;
-
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', res.header(origin));
+    res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', 'true');
-    console.log(res.headers);
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    console.log(res.headers);
     return res.end();
   }
   next();

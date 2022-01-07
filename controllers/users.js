f8a56cb80cf35d806c1255dc56d8618a5f4389c0
Dana <sundetbayeva.dana@yandex.ru>
2022-01-07 21:56:13 +0500

    from codevs to server

--- controllers/users.js
+++ controllers/users.js
@@ -114,8 +114,10 @@ const login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, verifyConst, { expiresIn: '7d' });
-      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true }).end();
-      res.status(200).send({data: user})
+      // res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true }).end();
+      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true });
+      res.status(200).send({ data: user });
+      return user;
     })
     .catch(() => {
       next(new Unauthorized('Неправильные почта или пароль'));
